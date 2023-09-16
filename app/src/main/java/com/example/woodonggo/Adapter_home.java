97193cc61775f2c45277f95f1a904a5498789328
SeooyeonJ <130852344+SeooyeonJ@users.noreply.github.com>
2023-09-16 20:53:52 +0900

    Merge remote-tracking branch 'origin/main'

--- app/src/main/java/com/example/woodonggo/Adapter_home.java
+++ app/src/main/java/com/example/woodonggo/Adapter_home.java
@@ -1,8 +1,13 @@
 package com.example.woodonggo;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -31,6 +36,39 @@ public class Adapter_home extends RecyclerView.Adapter<Adapter_home.ViewHolder> 
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         String text = localDataSet.get(position);
         holder.text.setText(text);
+
+        // more ImageView에 클릭 리스너 추가
+        holder.more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMenu(v);
+            }
+        });
+    }
+
+    private void showPopupMenu(View v) {
+        PopupMenu popupMenu = new PopupMenu(v.getContext(), v);
+        MenuInflater menuInflater = popupMenu.getMenuInflater();
+        menuInflater.inflate(R.menu.home_more_menu, popupMenu.getMenu());
+
+        //팝업 메뉴 아이템 클릭 이벤트
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int itemId = item.getItemId();
+                if(itemId == R.id.report_author) {
+                    //TODO: 작성자 신고
+                }
+                else if(itemId == R.id.report_post) {
+                    //TODO: 게시글 신고
+                }
+                else {
+                    //TODO: 채팅 걸기
+                }
+                return false;
+            }
+        });
+        popupMenu.show(); //팝업 메뉴 띄우기
     }
 
     @Override
@@ -38,13 +76,16 @@ public class Adapter_home extends RecyclerView.Adapter<Adapter_home.ViewHolder> 
         return localDataSet.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder { //뷰홀더 클래스
+    public static class ViewHolder extends RecyclerView.ViewHolder { // 뷰홀더 클래스
         private TextView text;
+        private ImageView more;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             text = itemView.findViewById(R.id.text);
+            more = itemView.findViewById(R.id.more);
         }
+
         public TextView getText() {
             return text;
         }
--- app/src/main/java/com/example/woodonggo/Home_Fragment_Personal.java
+++ app/src/main/java/com/example/woodonggo/Home_Fragment_Personal.java
@@ -1,9 +1,14 @@
 package com.example.woodonggo;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
