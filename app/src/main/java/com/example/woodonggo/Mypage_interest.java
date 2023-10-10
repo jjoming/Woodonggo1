9c094675137d2da924d9889a368e14e7b0ae696a
jjihong <128901284+jjihong@users.noreply.github.com>
2023-10-11 00:15:33 +0900

    관심목록 업데이트

--- app/src/main/java/com/example/woodonggo/Mypage_interest.java
+++ app/src/main/java/com/example/woodonggo/Mypage_interest.java
@@ -20,6 +20,9 @@ public class Mypage_interest extends AppCompatActivity {
     RecyclerView recyclerView;
     RadioGroup matchGroup;
     RadioButton all_btn, personal_btn, team_btn;
+    Mypage_interest_RecyclerView_Adapter adapter; // Adapter를 멤버 변수로 선언
+    private List<Model_interest> dataList; // 데이터 목록을 멤버 변수로 선언
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +38,8 @@ public class Mypage_interest extends AppCompatActivity {
         personal_btn = findViewById(R.id.personal_btn);
         team_btn = findViewById(R.id.team_btn);
         recyclerView = findViewById(R.id.recyclerView);
+        dataList = getDataList(); // 데이터 목록 초기화
+        adapter = new Mypage_interest_RecyclerView_Adapter(dataList); // 어댑터 초기화
 
         matchGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -47,16 +52,24 @@ public class Mypage_interest extends AppCompatActivity {
 
         // RecyclerView 설정
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        Mypage_interest_RecyclerView_Adapter adapter = new Mypage_interest_RecyclerView_Adapter(getDataList());
         recyclerView.setAdapter(adapter);
+
+        // 아이템 클릭 리스너 설정
+        adapter.setOnItemClickListener(new Mypage_interest_RecyclerView_Adapter.OnItemClickListener() {
+            @Override
+            public void onItemDeleteClick(int position) {
+                // 아이템 삭제 작업 수행
+                dataList.remove(position); // 데이터 목록에서도 해당 아이템 제거
+                adapter.notifyItemRemoved(position); // RecyclerView에 삭제된 것을 알림
+            }
+        });
 
     }
 
     private void updateRecyclerViewContent(int checkedId) {
 
-// 라디오 버튼에 따라 리사이클러뷰 내용 업데이트
+        // 라디오 버튼에 따라 리사이클러뷰 내용 업데이트
         List<Model_interest> filteredDataList = new ArrayList<>();
-        List<Model_interest> dataList = getDataList();
 
         if (checkedId == R.id.all_btn) {
             filteredDataList.addAll(dataList);
@@ -77,14 +90,17 @@ public class Mypage_interest extends AppCompatActivity {
         }
 
         // 어댑터에 필터링된 데이터 설정
-        Mypage_interest_RecyclerView_Adapter adapter = new Mypage_interest_RecyclerView_Adapter(filteredDataList);
-        recyclerView.setAdapter(adapter);
+        adapter.setData(filteredDataList);
+
+
 
     }
 
     private List<Model_interest> getDataList() {
         // 예제 데이터 생성
         List<Model_interest> dataList = new ArrayList<>();
+        dataList.add(new Model_interest(R.drawable.camera, "도구리", "30분 전", "오늘 공원에서 배드민턴 하실 분", true, false));
+        dataList.add(new Model_interest(R.drawable.camera, "도구리", "30분 전", "오늘 공원에서 배드민턴 하실 분", false, true));
         dataList.add(new Model_interest(R.drawable.camera, "도구리", "30분 전", "오늘 공원에서 배드민턴 하실 분", true, false));
 
         return dataList;
--- app/src/main/java/com/example/woodonggo/Mypage_interest_RecyclerView_Adapter.java
+++ app/src/main/java/com/example/woodonggo/Mypage_interest_RecyclerView_Adapter.java
@@ -1,10 +1,12 @@
 package com.example.woodonggo;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,6 +17,15 @@ import java.util.List;
 public class Mypage_interest_RecyclerView_Adapter extends RecyclerView.Adapter<Mypage_interest_RecyclerView_Adapter.ViewHolder> {
 
     private List<Model_interest> dataList; // 아이템 데이터 목록
+    private OnItemClickListener listener; // 인터페이스 추가
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    public interface OnItemClickListener {
+        void onItemDeleteClick(int position); // 아이템 삭제 이벤트를 처리하기 위한 콜백 메서드
+    }
 
     // 생성자를 통해 데이터 목록을 받아옵니다.
     public Mypage_interest_RecyclerView_Adapter(List<Model_interest> dataList) {
@@ -22,12 +33,13 @@ public class Mypage_interest_RecyclerView_Adapter extends RecyclerView.Adapter<M
     }
 
     // ViewHolder 클래스 정의
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder {
         public ImageView profileImage;
         public TextView tvName;
         public TextView uploadTime;
         public TextView tvTitle;
         public View divider;
+        public ImageButton moreBtn;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -36,6 +48,14 @@ public class Mypage_interest_RecyclerView_Adapter extends RecyclerView.Adapter<M
             uploadTime = itemView.findViewById(R.id.upload_time);
             tvTitle = itemView.findViewById(R.id.tv_title);
             divider = itemView.findViewById(R.id.divider);
+            moreBtn = itemView.findViewById(R.id.more_btn);
+
+            moreBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    showPopupMenu(v, getAdapterPosition());
+                }
+            });
         }
     }
 
@@ -69,5 +89,29 @@ public class Mypage_interest_RecyclerView_Adapter extends RecyclerView.Adapter<M
     @Override
     public int getItemCount() {
         return dataList.size();
+    }
+
+    private void showPopupMenu(View view, int position) {
+        if (listener != null && position != RecyclerView.NO_POSITION) {
+            PopupMenu popupMenu = new PopupMenu(view.getContext(), view);
+            popupMenu.inflate(R.menu.mypage_interest_menu);
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    if (item.getItemId() == R.id.menu_remove) {
+                        listener.onItemDeleteClick(position);
+                        return true;
+                    }
+                    return false;
+                }
+            });
+            popupMenu.show();
+        }
+    }
+
+    // setData 메서드 추가
+    public void setData(List<Model_interest> dataList) {
+        this.dataList = dataList;
+        notifyDataSetChanged();
     }
 }
