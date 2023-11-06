8d21caf90c76816f341a4b446a5756691a4c4053
minkyeongcho <jjoming129@naver.com>
2023-11-06 14:32:49 +0900

    LoginMain 추가

--- app/src/main/java/com/example/woodonggo/LoginSignup.java
+++ app/src/main/java/com/example/woodonggo/LoginSignup.java
@@ -24,9 +24,11 @@ import androidx.core.content.ContextCompat;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthMissingActivityForRecaptchaException;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
@@ -142,8 +144,68 @@ public class LoginSignup extends AppCompatActivity {
                     // todo : 인증번호 보내기
                     String ph = "+82";
                     ph += phone.substring(1);
+
+                    /*
+                    PhoneAuthOptions options =
+                            PhoneAuthOptions.newBuilder(mAuth)
+                                    .setPhoneNumber(ph)       // Phone number to verify
+                                    .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                                    .setActivity(LoginSignup.this)                 // (optional) Activity for callback binding
+                                    // If no activity is passed, reCAPTCHA verification can not be used.
+                                    .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                                    .build();
+                    PhoneAuthProvider.verifyPhoneNumber(options);
+
+                    mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+                        @Override
+                        public void onVerificationCompleted(@NonNull PhoneAuthCredential credential) {
+                            // This callback will be invoked in two situations:
+                            // 1 - Instant verification. In some cases the phone number can be instantly
+                            //     verified without needing to send or enter a verification code.
+                            // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                            //     detect the incoming verification SMS and perform verification without
+                            //     user action.
+                            Log.d(TAG, "onVerificationCompleted:" + credential);
+
+                            signInWithPhoneAuthCredential(credential);
+                        }
+
+                        @Override
+                        public void onVerificationFailed(@NonNull FirebaseException e) {
+                            // This callback is invoked in an invalid request for verification is made,
+                            // for instance if the the phone number format is not valid.
+                            Log.w(TAG, "onVerificationFailed", e);
+
+                            if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                                // Invalid request
+                            } else if (e instanceof FirebaseTooManyRequestsException) {
+                                // The SMS quota for the project has been exceeded
+                            } else if (e instanceof FirebaseAuthMissingActivityForRecaptchaException) {
+                                // reCAPTCHA verification attempted with null Activity
+                            }
+
+                            // Show a message and update the UI
+                        }
+
+                        @Override
+                        public void onCodeSent(@NonNull String verificationId,
+                                               @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                            // The SMS verification code has been sent to the provided phone number, we
+                            // now need to ask the user to enter the code and then construct a credential
+                            // by combining the code with a verification ID.
+                            Log.d(TAG, "onCodeSent:" + verificationId);
+
+                            // Save verification ID and resending token so we can use them later
+                            mVerificationId = verificationId;
+                            mResendToken = token;
+                        }
+                    };
+
+                     */
+                    /*
                     PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                        phone, // 사용자 전화번호
+                        ph, // 사용자 전화번호
                         60, // 타임아웃 시간
                         TimeUnit.SECONDS, // 타임아웃 시간 단위
                         LoginSignup.this, // Activity 또는 Context
@@ -170,6 +232,8 @@ public class LoginSignup extends AppCompatActivity {
                                 mVerificationId = verificationId;
                             }
                         });
+
+                     */
                 }
 
             }
