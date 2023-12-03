4dfc0abc627f68827d42f4a61909491f0b09f3b8
SeooyeonJ <130852344+SeooyeonJ@users.noreply.github.com>
2023-12-03 15:05:13 +0900

    로그인 화면 체크박스 커스텀 완료

--- app/src/main/java/com/example/woodonggo/LoginMain.java
+++ app/src/main/java/com/example/woodonggo/LoginMain.java
@@ -111,22 +111,22 @@ public class LoginMain extends AppCompatActivity {
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String id, passwd;
-
+                String id, pw;
                 //에디드텍스트에 입력된 아이디, 패스워드 받아와서 String으로 변환
                 id = id_edit.getText().toString();
-                passwd = pw_edit.getText().toString();
+                pw = pw_edit.getText().toString();
+                if (TextUtils.isEmpty(id) || TextUtils.isEmpty(pw)) {
+                    pw = pw_edit.getText().toString();
 
-                if (TextUtils.isEmpty(id) || TextUtils.isEmpty(passwd)) {
-                    Toast.makeText(LoginMain.this, "ID와 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show();
-                    return;
-                } else login(id,passwd);
+                    if (TextUtils.isEmpty(id) || TextUtils.isEmpty(pw)) {
+                        Toast.makeText(LoginMain.this, "ID와 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                        return;
+                    } else login(id,pw);
+                } else login(id,pw);
 
                 // todo : 아이디 패스워드 서버와 일치하는지 확인
             }
         });
-
-
         findId.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -134,7 +134,6 @@ public class LoginMain extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
         findPw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -142,7 +141,6 @@ public class LoginMain extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
         signUpTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -150,8 +148,6 @@ public class LoginMain extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
         kakao_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -163,9 +159,6 @@ public class LoginMain extends AppCompatActivity {
                 }
             }
         });
-
-
-
         naver_login.setOAuthLogin(new OAuthLoginCallback() {
             @Override
             public void onSuccess() {
@@ -175,7 +168,6 @@ public class LoginMain extends AppCompatActivity {
                 String header = "Bearer " + token;
                 Map<String, String> requestHeaders = new HashMap<>();
                 requestHeaders.put("Authorization", header);
-
                 // 로그인 성공 시 처리
                 new Thread(() -> {
                     responseBody = get(apiUrl, requestHeaders);
@@ -188,69 +180,57 @@ public class LoginMain extends AppCompatActivity {
                         finalphone = mobile;
                         checkIfUserExistsInFirestore(id);
                         // Firestore에 사용자 ID가 있는지 확인
-
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 }).start();
-
             }
-
             @Override
             public void onFailure(int httpStatus, String message) {
                 // 로그인 실패 시 처리
             }
-
             @Override
             public void onError(int errorCode, String message) {
                 // 로그인 에러 시 처리
             }
         });
-
-
-
     }
-
     private void login(String id, String pw) {
         DocumentReference userRef, autoLogin;
         userRef = db.collection("User").document(id);
-
         userRef.get().addOnCompleteListener(task -> {
-           if(task.isSuccessful()) {
-               DocumentSnapshot document = task.getResult();
-               if(document.exists()) {
-                   String storedPw = document.getString("passwd");
-                   if (pw.equals(storedPw)) {
-                       SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(LoginMain.this);
-                       preferences.edit().putBoolean(PREF_AUTO_LOGIN, autologin_chk.isChecked()).apply();
-                       SharedPreferences.Editor editor = preferences.edit();
-                       Log.d("HONG2",id);
-                       editor.putString("userId", id);
-                       editor.apply();
-                       Intent intent = new Intent(LoginMain.this, MainActivity.class);
-                       intent.putExtra("UserId",id);
-                       startActivity(intent);
-                       finish();
-                   } else {
-                       Toast.makeText(LoginMain.this,"비밀번호가 일치하지 않습니다.",Toast.LENGTH_SHORT).show();
-                   }
-               } else {
-                   Toast.makeText(LoginMain.this,"해당 ID가 존재하지 않습니다.",Toast.LENGTH_SHORT).show();
-               }
-           }else {
-               Toast.makeText(LoginMain.this, "사용자 정보 확인 중 오류 발생", Toast.LENGTH_SHORT).show();
-           }
+            if(task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if(document.exists()) {
+                    String storedPw = document.getString("passwd");
+                    if (pw.equals(storedPw)) {
+                        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(LoginMain.this);
+                        preferences.edit().putBoolean(PREF_AUTO_LOGIN, autologin_chk.isChecked()).apply();
+                        SharedPreferences.Editor editor = preferences.edit();
+                        Log.d("HONG2",id);
+                        editor.putString("userId", id);
+                        editor.apply();
+                        Intent intent = new Intent(LoginMain.this, MainActivity.class);
+                        intent.putExtra("UserId",id);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(LoginMain.this,"비밀번호가 일치하지 않습니다.",Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(LoginMain.this,"해당 ID가 존재하지 않습니다.",Toast.LENGTH_SHORT).show();
+                }
+            }else {
+                Toast.makeText(LoginMain.this, "사용자 정보 확인 중 오류 발생", Toast.LENGTH_SHORT).show();
+            }
         });
     }
-
     // Firestore에서 사용자 확인 메서드
     private void checkIfUserExistsInFirestore(String userId) {
         checkIfUserExistsInFirestore(userId,null);
     }
-
     private void checkIfUserExistsInFirestore(String userId, String profile) {
         DocumentReference userRef = db.collection("User").document(userId);
-
         userRef.get().addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
                 DocumentSnapshot document = task.getResult();
@@ -273,13 +253,12 @@ public class LoginMain extends AppCompatActivity {
                         Log.d("HONG",userId);
                     }
                     gotosignup2(userId);
-            }
+                }
                 // Firestore에서 사용자 확인 중 오류 발생
                 // 오류 처리를 수행하거나 필요에 따라 처리
             }
         });
     }
-
     private void gotosignup2(String userId) {
         gotosignup2(userId , null);
     }
@@ -295,8 +274,6 @@ public class LoginMain extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
-
-
     // 네이버로그인 안될경우.
     private static String get(String apiUrl, Map<String, String> requestHeaders) {
         HttpURLConnection con = connect(apiUrl);
@@ -305,7 +282,6 @@ public class LoginMain extends AppCompatActivity {
             for (Map.Entry<String, String> header : requestHeaders.entrySet()) {
                 con.setRequestProperty(header.getKey(), header.getValue());
             }
-
             int responseCode = con.getResponseCode();
             if (responseCode == HttpURLConnection.HTTP_OK) {
                 return readBody(con.getInputStream());
@@ -318,7 +294,6 @@ public class LoginMain extends AppCompatActivity {
             con.disconnect();
         }
     }
-
     private static HttpURLConnection connect(String apiUrl) {
         try {
             URL url = new URL(apiUrl);
@@ -329,7 +304,6 @@ public class LoginMain extends AppCompatActivity {
             throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
         }
     }
-
     private static String readBody(InputStream body) {
         InputStreamReader streamReader = new InputStreamReader(body);
         try (BufferedReader lineReader = new BufferedReader(streamReader)) {
@@ -343,7 +317,6 @@ public class LoginMain extends AppCompatActivity {
             throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
         }
     }
-
     public void login(){
         String TAG = "login()";
         UserApiClient.getInstance().loginWithKakaoTalk(LoginMain.this, (oAuthToken, error) -> {
@@ -356,7 +329,6 @@ public class LoginMain extends AppCompatActivity {
             return null;
         });
     }
-
     public void accountLogin(){
         String TAG = "accountLogin()";
         UserApiClient.getInstance().loginWithKakaoAccount(LoginMain.this, (oAuthToken, error) -> {
@@ -388,13 +360,10 @@ public class LoginMain extends AppCompatActivity {
                 String profile = user.getKakaoAccount().getProfile().getThumbnailImageUrl();
                 id = id + "masterkey";
                 checkIfUserExistsInFirestore(id,profile);
-                 // 현재 액티비티를 종료하여 뒤로 가기 버튼으로 다시 돌아오지 않도록 합니다.
+                // 현재 액티비티를 종료하여 뒤로 가기 버튼으로 다시 돌아오지 않도록 합니다.
             }
             return null;
         });
         Log.i(TAG, "getUserInfo() 종료");
     }
-
-
-
 }
